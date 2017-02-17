f8fbe452eaf567597e7ef85cb29f91b4bbce3d4e
Jakub <jakub.oblizajek@mensa.pl>
2017-02-17 03:19:07 +0100

    Create basic view and URLConf.

--- booksrev/urls.py
+++ booksrev/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from books.views import list_books
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', list_books, name='books'),
 ]
 
 if settings.DEBUG:
