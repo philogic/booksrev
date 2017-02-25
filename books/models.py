11d18293e7a4c2ea05f8cce850d060226948e320
Jakub <jakub.oblizajek@mensa.pl>
2017-02-25 03:19:56 +0100

    Add models tests

--- books/models.py
+++ books/models.py
@@ -5,6 +5,7 @@ from django.utils.timezone import now
 
 # Create your models here.
 
+
 class Book(models.Model):
     title = models.CharField(max_length=150)
     authors = models.ManyToManyField('Author', related_name='books')
@@ -23,9 +24,6 @@ class Book(models.Model):
         if (self.review and self.date_reviewed is None):
             self.date_reviewed = now()
         super(Book, self).save(*args, **kwargs)
-
-
-
 
 
 class Author(models.Model):
